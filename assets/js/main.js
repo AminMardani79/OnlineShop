3dea16232194a6116bdf5d342981b6ad32f0d9fc
AminMardani79 <76900072+AminMardani79@users.noreply.github.com>
2022-02-01 23:36:49 +0330

    top header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 /* feather icon */
 feather.replace();
+
+const searchInput = document.querySelector(".top-header__input-search");
+const searchIcon = document.querySelector(".top-header__search-btn svg");
+searchInput.addEventListener("focus", () => {
+    searchIcon.style.transform = "rotate(-90deg)";
+});
+searchInput.addEventListener("blur", () => {
+    searchIcon.style.transform = "rotate(0)";
+})
