fa57d8e3b5ca8277f2edec22cd67338d3a282eaa
AminMardani79 <76900072+AminMardani79@users.noreply.github.com>
2022-02-03 20:45:12 +0330

    mega-menu slide

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 /* feather icon */
 feather.replace();
 
+
+/* header search */
 const searchInput = document.querySelector(".top-header__input-search");
 const searchIcon = document.querySelector(".top-header__search-btn svg");
 searchInput.addEventListener("focus", () => {
@@ -9,3 +11,16 @@ searchInput.addEventListener("focus", () => {
 searchInput.addEventListener("blur", () => {
     searchIcon.style.transform = "rotate(0)";
 })
+
+
+const megaOpen = document.querySelector(".top-header__hambergur-menu");
+const megaMenu = document.querySelector(".mega-menu");
+const overlay = document.querySelector('.main-overlay');
+megaOpen.onclick = function() {
+    megaMenu.classList.add('mega-menu_active');
+    overlay.classList.add('main-overlay_active');
+}
+overlay.onclick = function() {
+    megaMenu.classList.remove('mega-menu_active');
+    overlay.classList.remove('main-overlay_active');
+}
